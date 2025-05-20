ceb0d877211d6d961c4a2fc45e7e925b25d4c014
jean leon <jean_leo2000@hotmail.com>
2025-05-20 10:35:38 -0500

    perf: optimize bundle size with code splitting and terser minification

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,24 @@ export default defineConfig({
   plugins: [react({
     jsxRuntime: 'automatic'
   })],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          form: ['react-hook-form'],
+          ui: ['sonner', 'swiper']
+        }
+      }
+    }
+  },
   css: {
     preprocessorOptions: {
       scss: {
